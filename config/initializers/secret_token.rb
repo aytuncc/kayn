f34c4a4910dbb97925c20ff891bf276390cc52b7
Aytunc Ozturk <aytuncozturk@gmail.com>
2015-04-24 23:34:35 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaynakli::Application.config.secret_key_base = '316cd885a75c90db0355a420b419e40e25989d6efdf9baeb3e117b88f80105d966e46c45327c616df55321552cb0f779a91cdc9c44c0f5c2194448f41fc0edb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaynakli::Application.config.session_store :cookie_store, key: '_kaynakli_session'
